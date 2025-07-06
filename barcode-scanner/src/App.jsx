470527d9b50d7474049220dcd8e6fa13ebc9a531
Ruben <ruben.ryckaert89@gmail.com>
2025-07-06 22:43:41 +0200

    Login didn't work but changed scanning

--- barcode-scanner/src/App.jsx
+++ barcode-scanner/src/App.jsx
@@ -3,35 +3,17 @@ import './App.css'
 import Header from './header'
 import Scanner from './Scanner'
 import ButtonPing from './ButtonPing'
-import Login from './Login'
-import { AuthProvider, useAuth } from './AuthContext.jsx'
 
-function AppContent() {
-  const { user, logout } = useAuth();
-
-  // If the user is not logged in, show the Login screen
-  if (!user) {
-    return <Login />;
-  }
-
+function App(){
   // Otherwise, show the main application content
   return (
     <div className="App">
       <Logo />
       <Header />
-      <p>Welcome, {user.username}! <button onClick={logout}>Logout</button></p>
       <Scanner />
       <ButtonPing/>
     </div>
   );
-}
-
-function App() {
-  return (
-    <AuthProvider>
-      <AppContent />
-    </AuthProvider>
-  )
 }
 
 export default App
--- barcode-scanner/src/ButtonPing.jsx
+++ barcode-scanner/src/ButtonPing.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function ButtonPing(){
 
     const handlePing = () => {
